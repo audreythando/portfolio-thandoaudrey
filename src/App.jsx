77464ee7b65f941b4eb36522077e9770b806b804
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2023-07-30 23:00:50 +0200

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
+import Footer from './components/footer/Footer';
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BsLinkedin } from 'react-icons/bs'
+import{FaGithub} from 'react-icons/fa'
+import {SiGmail} from 'react-icons/si'
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target='_blank' rel='noreferrer'><BsLinkedin/> </a>
+        <a href="https://github.com" target='_blank'  rel='noreferrer'><FaGithub/></a>
+        <a href="https://gmail.com" target='_blank'  rel='noreferrer'><SiGmail/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
